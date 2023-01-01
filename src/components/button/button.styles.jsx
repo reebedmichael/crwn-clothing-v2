b1a2591c6d65cfdd17406641d81fef9165ff7bdd
Michael de Beer <debeermichael17@gmail.com>
2023-01-01 18:33:59 +0200

    new

--- src/components/button/button.styles.jsx
+++ src/components/button/button.styles.jsx
@@ -1,4 +1,6 @@
-.button-container {
+import styled from "styled-components";
+
+export const BaseButton = styled.button`
   min-width: 165px;
   width: auto;
   height: 50px;
@@ -22,7 +24,10 @@
     border: 1px solid black;
   }
 
-  &.google-sign-in {
+  
+`
+  
+export const GoogleSignInButton = styled(BaseButton)`
     background-color: #4285f4;
     color: white;
 
@@ -30,9 +35,10 @@
       background-color: #357ae8;
       border: none;
     }
-  }
-
-  &.inverted {
+  
+`
+  
+export const InvertedButton = styled(BaseButton)`
     background-color: white;
     color: black;
     border: 1px solid black;
@@ -42,5 +48,4 @@
       color: white;
       border: none;
     }
-  }
-}
+`
